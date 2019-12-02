b23a8accdd5d85c748d7f71b4d1f8f46c43aa97f
ArashBayatDev <Arash.Bayat@csiro.au>
2019-12-01 16:51:29 -0800

    add VCF2EPI

--- runme.sh
+++ runme.sh
@@ -9,6 +9,9 @@ set -x
 #An example how to convert GAMETES simulated data to the format accepted by BitEpi and transposed plink file (for MPI3SNP and BOOST)
 bash GAMETES_2_EPI.sh sampleData/GAMETES_Example_Output
 
+#An example how to convert VCF file to BitEpi input file.
+bash VCF_2_EPI.sh sampleData/Hipster.vcf.bgz sampleData/Hipster.tsv 2
+
 #Examles on how to run cluster mode (not implemented yet)
 #./BitEpi.o  -i sampleData/data.csv -o sampleData/out4 -c -j 6 -f 3 -t 2 -a4 100
 
