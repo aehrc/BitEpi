b5ec85c07fb1a47348badf0f16b4832612bf6c6a
Milindi-Kodikara <s3667779@student.rmit.edu.au>
2020-01-24 16:26:28 +1100

    Reformat code

--- AdvanceVisualisation/CytoscapeIntegration.py
+++ AdvanceVisualisation/CytoscapeIntegration.py
@@ -1,17 +1,6 @@
 import os
-import sys
-
 from py2cytoscape.data.cyrest_client import CyRestClient
-from py2cytoscape.data.util_network import NetworkUtil as util
-from py2cytoscape.data.style import StyleUtil as s_util
-import py2cytoscape.cytoscapejs as renderer
-
 from IPython.display import Image
-import networkx as nx
-from networkx.drawing import nx_pydot as pyd
-import igraph as ig
-import numpy as np
-import pandas as pd
 import json
 
 
